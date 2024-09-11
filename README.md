3c296f7012724b4781ebd3b3e0933946eb72c990
Matthew Billman <mgbvox@gmail.com>
2024-09-11 11:52:24 -0400

    begin building out cnn layer w/ config struct

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Candle-MNIST
 
 An exploration of Huggingface's Candle ML library.
+
+To run:
+
+```bash
+cargo run --example mnist
+```
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,130 @@
-use candle_core::{Device, Shape, Tensor, Result};
-use candle_core::op::Op;
+use candle_core::{Device, Shape, Tensor, Result, DType};
+use candle_nn::{Conv2d, Dropout, Linear, Module, VarBuilder};
+use anyhow;
+
+const MODEL_DTYPE: DType = DType::F32;
 
 fn default_device() -> Device {
     Device::new_metal(0).expect("Unable to create metal device.")
 }
 
 
-struct Linear {
-    weights: Tensor,
-    bias: Tensor,
+enum ModelConfig {
+    CNNLayerConfig,
 }
 
-impl Linear {
-    fn new<S: Into<Shape>>(shape: S,
-            device: Option<Device>,
-           weights_initializer: Option<dyn Fn()->Result<Tensor>>,
-           bias_initializer: Option<dyn Fn()->Result<Tensor>>
-    ) -> Self {
+#[derive(Debug)]
+pub struct LinearConfig {
+    in_shape: usize,
+    out_shape: usize,
+}
 
-        
 
-        let weights = match weights_initializer {
-            None => Tensor::randn(0f32, 1.0, shape, &device.unwrap_or(default_device())),
-            Some(callback) => callback()
-        };
+#[derive(Debug)]
+pub struct ConvConfig {
+    in_shape: usize,
+    out_shape: usize,
+    kernel_size: usize,
+}
 
-        let bias = match bias_initializer {
-            None => Tensor::randn(0f32, 1.0, shape, &device.unwrap_or(default_device())),
-            Some(init) => init()
-        };
+#[derive(Debug)]
+pub struct CNNLayerConfig {
+    layer_name: String,
+    conv_layer: ConvConfig,
+    linear_config: LinearConfig,
+    dropout: f32,
+}
 
-        Linear {
-            weights:
-        }
 
+#[derive(Debug)]
+pub struct CNNLayer {
+    config: CNNLayerConfig,
+    conv: Conv2d,
+    linear: Linear,
+    dropout: Dropout,
+}
+
+
+impl CNNLayer {
+    fn new(vs: VarBuilder, config: CNNLayerConfig) -> Result<Self> {
+        let conv = candle_nn::conv2d(
+            config.conv_layer.in_shape,
+            config.conv_layer.out_shape,
+            config.conv_layer.kernel_size,
+            Default::default(),
+            vs.push_prefix(format!("{}-conv2D", config.layer_name)),
+        )?;
+
+        let linear = candle_nn::linear(
+            config.linear_config.in_shape,
+            config.linear_config.out_shape,
+            vs.push_prefix(format!("{}-linear", config.layer_name)),
+        )?;
+
+        let dropout = candle_nn::Dropout::new(config.dropout);
+
+        Ok(Self {
+            config,
+            conv,
+            linear,
+            dropout,
+        })
+    }
+    fn forward(&self, xs: &Tensor) -> Result<Tensor> {
+        todo!()
     }
 }
 
-fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+#[cfg(test)]
+mod tests {
+    use candle_core::DType;
+    use candle_nn::VarMap;
+    use super::*;
+
+
+    fn build_cnn_layer() -> Result<CNNLayer> {
+        let device = default_device();
+        let varmap = VarMap::new();
+        let vars = VarBuilder::from_varmap(&varmap.clone(), MODEL_DTYPE, &device);
+
+        let config = CNNLayerConfig {
+            layer_name: "foo".to_string(),
+            conv_layer: ConvConfig {
+                in_shape: 5,
+                out_shape: 5,
+                kernel_size: 5,
+            },
+            linear_config: LinearConfig {
+                in_shape: 5,
+                out_shape: 5,
+            },
+            dropout: 0.5,
+        };
+
+
+        let layer = CNNLayer::new(vars.clone(), config)?;
+
+        Ok(layer)
+    }
+
+    #[test]
+    fn test_build_cnn_layer() -> anyhow::Result<()> {
+        let layer = build_cnn_layer()?;
+
+        println!("layer: {:?}", layer);
+        Ok(())
+    }
+
+    #[test]
+    fn test_cnn_forward() -> anyhow::Result<()> {
+        let layer = build_cnn_layer()?;
+        // let input
+        Ok(())
+    }
+}
+
+
+fn main() -> anyhow::Result<()> {
     if let device = Device::new_metal(0)? {
         println!("Device created.");
         let a = Tensor::randn(0f32, 1., (2, 3), &device)?;
@@ -48,9 +135,8 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
         println!("{:?}", c);
     } else {
-        println!("Device not make uhoh.");
+        println!("Could not create metal device.");
     }
-
 
     Ok(())
 }
